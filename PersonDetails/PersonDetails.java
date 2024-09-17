a1a2ede89426d5ed1964303335c64c3ca218eb7a
Kushal <kushalobroy@gmail.com>
2024-09-17 11:35:06 +0530

    initial commit

---
+++ PersonDetails/PersonDetails.java
@@ -0,0 +1,40 @@
+/* Write a Java program to create a class called "Person" with a name and age attribute. Create two instances of the "Person" class, set their attributes using the constructor, and print their name and age. */
+
+class Person{
+    private String name;
+    private int age;
+    // Constructor
+    public Person(String name, int age){
+        this.name = name;
+        this.age=age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+}
+
+public class PersonDetails{
+    public static void main(String[] args){
+        //System.out.println("Kushal");
+        Person p1 = new Person("Kushal Obroy",22);
+        Person p2 = new Person("Kusum", 21);
+        System.out.println(p1.getName()+" "+p1.getAge()+" years old");
+        System.out.println(p2.getName()+" "+p2.getAge()+" years old");
+
+        p1.setAge(0);
+        p2.setName("Neha");
+        System.out.println("Updated Name & Age :");
+        System.out.println(p1.getName()+" "+p1.getAge()+" years old");
+        System.out.println(p2.getName()+" "+p2.getAge()+" years old");
+
+    }
+}
