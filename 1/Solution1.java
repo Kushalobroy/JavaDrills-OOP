12b2c44d3795e19505356dbdf854ef307ada14ee
Kushal <kushalobroy@gmail.com>
2024-10-21 19:28:49 +0530

    initial commit

--- 1/Solution1.java
+++ 1/Solution1.java
@@ -2,13 +2,13 @@
 import java.util.Scanner;
 import java.util.*;
 
-class Employee2{
+class Employee3{
     private int employeeId;
     private String employeeName;
     private int age;
     private char gender;
     private double salary;
-    public Employee2(int employeeId, String employeeName, int age, char gender, double salary){
+    public Employee3(int employeeId, String employeeName, int age, char gender, double salary){
         this.employeeId = employeeId;
         this.employeeName = employeeName;
         this.age = age;
@@ -30,7 +30,7 @@ public class Solution1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Employee2[] employees = new Employee2[n];
+        Employee3[] employees = new Employee3[n];
         for(int i=0;i<n;i++){
             int employeeId = sc.nextInt();
             sc.nextLine();
@@ -40,12 +40,12 @@ public class Solution1{
             char gender = sc.next().charAt(0);
             sc.nextLine();
             double salary = sc.nextDouble();
-            employees[i] = new Employee2(employeeId, employeeName, age, gender, salary);
+            employees[i] = new Employee3(employeeId, employeeName, age, gender, salary);
         }
         int ageInput = sc.nextInt();
-        Employee2[] EmployeeWithSecondLowestSalary = getEmployeeWithSecondLowestSalary(employees);
+        Employee3[] EmployeeWithSecondLowestSalary = getEmployeeWithSecondLowestSalary(employees);
         if(EmployeeWithSecondLowestSalary != null){
-            for(Employee2 emp : EmployeeWithSecondLowestSalary){
+            for(Employee3 emp : EmployeeWithSecondLowestSalary){
                 System.out.println(emp.getEmployeeId() +"#"+emp.getEmployeeName());
             }
         }else System.out.println("Null");
@@ -54,28 +54,28 @@ public class Solution1{
         sc.close();
 
     }
-    public static int getCountEmployeesOnAge(Employee2[] employees,int ageInput){
+    public static int getCountEmployeesOnAge(Employee3[] employees,int ageInput){
          int count=0; 
         
-             for(Employee2 e: employees){
+             for(Employee3 e: employees){
 if(e.getAge() == ageInput) count++;
              }
           
           return count;
     }
-    public static Employee2[] getEmployeeWithSecondLowestSalary(Employee2[] employees){
-        if(employees == null || employees.length <2) return new Employee2[0];
+    public static Employee3[] getEmployeeWithSecondLowestSalary(Employee3[] employees){
+        if(employees == null || employees.length <2) return new Employee3[0];
         Arrays.sort(employees, (e1, e2) -> Double.compare(e1.getSalary(), e2.getSalary()));
         double lowestSalary = employees[0].getSalary();
         double secondLowestSalary = -1;
-        for(Employee2 employee : employees){
+        for(Employee3 employee : employees){
             if(employee.getSalary()>lowestSalary){
                 secondLowestSalary = employee.getSalary();
                 break;
             }
         }
-        if(secondLowestSalary == -1) return new Employee2[0];
+        if(secondLowestSalary == -1) return new Employee3[0];
         double finalSecondLowestSalary = secondLowestSalary;
-        return Arrays.stream(employees).filter(e -> e.getSalary() == finalSecondLowestSalary).toArray(Employee2[]::new);
+        return Arrays.stream(employees).filter(e -> e.getSalary() == finalSecondLowestSalary).toArray(Employee3[]::new);
     }
 }
