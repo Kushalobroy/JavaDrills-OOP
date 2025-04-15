be73c7a0c7fa0f046e38acdf15b699d05e296df6
Kushal <kushalobroy@gmail.com>
2025-04-15 16:20:22 +0530

    initial commit

--- 48/Solution48.java
+++ 48/Solution48.java
@@ -1,10 +1,11 @@
+import java.util.*;
 public class Solution48{
-    class BankAccount{
+    static class BankAccount{
         private int accountNumber;
         private String accountHolderName;
         private double balance;
 
-        public  BankAccount(int accountNumber, String accountHolderName, double balance){
+        public BankAccount(int accountNumber, String accountHolderName, double balance){
             this.accountNumber = accountNumber;
             this.accountHolderName = accountHolderName;
             this.balance = balance;
@@ -34,15 +35,53 @@ public class Solution48{
         BankAccount[] bankaccounts = new BankAccount[3];
         for(int i=0;i<3;i++){
             int a = sc.nextInt();
+            sc.nextLine();
             String b = sc.nextLine();
             double c = sc.nextDouble();
+            sc.nextLine();
             bankaccounts[i] = new BankAccount(a,b,c);
         }
-        int a1 = sc.nextInt();
-        double b1 = sc.nextDouble();
-        int a2 = sc.nextInt();
-        double b2 = sc.nextDouble();
-
+        int a1 = sc.nextInt(); sc.nextLine();
+        double b1 = sc.nextDouble();sc.nextLine();
+        int a2 = sc.nextInt();sc.nextLine();
+        double b2 = sc.nextDouble();sc.nextLine();
         
+        double c1 = withdraw(bankaccounts, a1, b1);
+        if(c1 != -1){
+            System.out.println("Withdrawn successfully. Remaining balance: " + c1);
+        }
+        else{
+            System.out.println("Sorry - Insufficient balance");
+        }
+        double c2 = deposite(bankaccounts, a2, b2);
+        if(c2 != -1){
+            System.out.println("Deposited successfully. New balance: " + c2);
+        }
+        else{
+            System.out.println("Sorry - Account not found");
+        }
+        
+    }
+    public static double deposite(BankAccount[] bankaccounts, int accountNumber, double amount){
+        for(int i=0;i<bankaccounts.length;i++){
+            if(bankaccounts[i].getAcc() == accountNumber){
+                bankaccounts[i].setBalance(bankaccounts[i].getBalance() + amount);
+                return bankaccounts[i].getBalance() + amount;
+                
+            }
+        }
+        return -1;
+    }
+    public static double withdraw(BankAccount[] bankaccounts, int accountNumber, double amount){
+        for(int i=0;i<bankaccounts.length;i++){
+            if(bankaccounts[i].getAcc() == accountNumber){
+                if(bankaccounts[i].getBalance() >= amount){
+                    bankaccounts[i].setBalance(bankaccounts[i].getBalance() - amount);
+                    return  bankaccounts[i].getBalance() - amount;
+                    
+                }
+            }
+        }
+        return -1;
     }
 }
